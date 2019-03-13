c47d6e94dbd02d245d0a20dc855d412995997f20
Omar Nassef <omar.nassef@kcl.ac.uk>
2019-03-13 22:37:20 +0000

    changed GPS coordinates in TO to match documenation for correct caulcations

--- to/CreateTrajectory.cpp
+++ to/CreateTrajectory.cpp
@@ -35,6 +35,13 @@ int ProcessedSpeedtoRoadUserSpeed(int speed){
 	return speed / 100;
 }
 
+float RoadUserGPStoProcessedGPS(float point){
+	return point / pow(10,6);
+}
+float ProcessedGPStoRoadUserGPS(float point){
+	return point * pow(10,6);
+}
+
 // TODO
 
 // 1. spacing for cars (remove for shorter training time)
@@ -42,16 +49,16 @@ int ProcessedSpeedtoRoadUserSpeed(int speed){
 
 pair<RoadUser*,RoadUser*> getClosestFollowingandPreceedingCars(RoadUser * merging_car,std::vector<RoadUser*> close_by){
   RoadUser * closest_following = new RoadUser();
-  closest_following->setLongitude(merging_car->getLongitude()-10000); //check TODO
-  closest_following->setLatitude(merging_car->getLatitude()-10000); // check
+  closest_following->setLongitude(RoadUserGPStoProcessedGPS(merging_car->getLongitude()-10000)); //check TODO
+  closest_following->setLatitude(RoadUserGPStoProcessedGPS(merging_car->getLatitude()-10000)); // check
   closest_following->setSpeed(merging_car->getSpeed());
   closest_following->setWidth(merging_car->getWidth());
   closest_following->setLength(merging_car->getLength());
   closest_following->setAcceleration(merging_car->getAcceleration());
   closest_following->setLanePosition(merging_car->getLanePosition()+1);
   RoadUser * closest_preceeding = new RoadUser();
-  closest_preceeding->setLongitude(merging_car->getLongitude()+10000); // check
-  closest_preceeding->setLatitude(merging_car->getLatitude()+10000); //check
+  closest_preceeding->setLongitude(RoadUserGPStoProcessedGPS(merging_car->getLongitude()+10000)); // check
+  closest_preceeding->setLatitude(RoadUserGPStoProcessedGPS(merging_car->getLatitude()+10000)); //check
   closest_preceeding->setSpeed(merging_car->getSpeed());
   closest_preceeding->setWidth(merging_car->getWidth());
   closest_preceeding->setLength(merging_car->getLength());
@@ -63,12 +70,12 @@ pair<RoadUser*,RoadUser*> getClosestFollowingandPreceedingCars(RoadUser * mergin
 
   for(RoadUser * close_car : close_by){
     if(close_car->getLatitude() < merging_car->getLatitude() && close_car->getLongitude() < merging_car->getLongitude()){ //closest following
-      if(distanceEarth(close_car->getLatitude(),close_car->getLongitude(),merging_car->getLatitude(),merging_car->getLongitude()) < minFollowing){
+      if(distanceEarth(RoadUserGPStoProcessedGPS(close_car->getLatitude()),RoadUserGPStoProcessedGPS(close_car->getLongitude()),RoadUserGPStoProcessedGPS(merging_car->getLatitude()),RoadUserGPStoProcessedGPS(merging_car->getLongitude())) < minFollowing){
           closest_following = close_car;
       }
     }
     if (close_car->getLatitude() > merging_car->getLatitude() && close_car->getLongitude() > merging_car->getLongitude()){ //closest preceeding
-      if(distanceEarth(close_car->getLatitude(),close_car->getLongitude(),merging_car->getLatitude(),merging_car->getLongitude()) < minPreceeding){
+      if(distanceEarth(RoadUserGPStoProcessedGPS(close_car->getLatitude()),RoadUserGPStoProcessedGPS(close_car->getLongitude()),RoadUserGPStoProcessedGPS(merging_car->getLatitude()),RoadUserGPStoProcessedGPS(merging_car->getLongitude())) < minPreceeding){
         closest_preceeding = close_car;
       }
     }
@@ -165,7 +172,7 @@ at::Tensor GetStateFromActions(at::Tensor action_Tensor,at::Tensor stateTensor){
 			auto final_velocity = state[0][4].item<float>() + 0.035 * (state[0][4].item<float>() + state[0][5].item<float>() * 0.035);
 			auto final_acceleration = (pow(final_velocity,2) - pow(state[0][4].item<float>(),2)) / 2 * (0.5 * (state[0][4].item<float>() + final_velocity) * 0.035);
 			auto displacement = final_velocity * 0.035 + 0.5 * (final_acceleration * 0.035 * 0.035);
-			auto angle = (int(state[0][19].item<float>()) + 180) % 360;
+			auto angle = int(state[0][19].item<float>());
 			auto new_x = state[0][0].item<float>() + displacement * cos((angle * M_PI)/ 180);
 			auto new_y = state[0][1].item<float>() + displacement * sin((angle * M_PI)/ 180);
 			stateTensor[0][0] = new_x;
@@ -178,7 +185,7 @@ at::Tensor GetStateFromActions(at::Tensor action_Tensor,at::Tensor stateTensor){
 		auto final_velocity = state[0][4].item<float>() - 0.035 * (state[0][4].item<float>() + state[0][5].item<float>() * 0.035);
 		auto final_acceleration = (pow(final_velocity,2) - pow(state[0][4].item<float>(),2)) / 2 * (0.5 * (state[0][4].item<float>() + final_velocity) * 0.035);
 		auto displacement = final_velocity * 0.035 + 0.5 * (final_acceleration * 0.035 * 0.035);
-		auto angle = (int(state[0][19].item<float>()) + 180) % 360;
+		auto angle = int(state[0][19].item<float>());
 		auto new_x = state[0][0].item<float>() + displacement * cos((angle * M_PI)/ 180);
 		auto new_y = state[0][1].item<float>() + displacement * sin((angle * M_PI)/ 180);
 		stateTensor[0][0] = new_x;
@@ -209,7 +216,7 @@ at::Tensor GetStateFromActions(at::Tensor action_Tensor,at::Tensor stateTensor){
 		return stateTensor;
 	} else if(doNothing_tensor == actionTensor.item<int>()){
 			auto displacement = state[0][4].item<float>() * 0.035 + 0.5 * (state[0][5].item<float>() * 0.035 * 0.035);
-	   	auto angle = (int(state[0][19].item<float>()) + 180) % 360;
+	   	auto angle = int(state[0][19].item<float>());
 	    auto new_x = state[0][0].item<float>() + displacement * cos((angle * M_PI)/ 180);
 	    auto new_y = state[0][1].item<float>() + displacement * sin((angle * M_PI)/ 180);
 	    stateTensor[0][0] = new_x;
@@ -235,25 +242,25 @@ vector<float> RoadUsertoModelInput(RoadUser * merging_car,vector<pair<RoadUser*,
   	}
 	}
 
-    mergingCar.push_back(merging_car->getLatitude());
-    mergingCar.push_back(merging_car->getLongitude());
+    mergingCar.push_back(RoadUserGPStoProcessedGPS(merging_car->getLatitude()));
+    mergingCar.push_back(RoadUserGPStoProcessedGPS(merging_car->getLongitude()));
     mergingCar.push_back(merging_car->getLength());
     mergingCar.push_back(merging_car->getWidth());
     mergingCar.push_back(RoadUserSpeedtoProcessedSpeed(merging_car->getSpeed()));
     mergingCar.push_back(merging_car->getAcceleration());
-    mergingCar.push_back(distanceEarth(merging_car->getLongitude(),merging_car->getLatitude(),x.first->getLongitude(),x.first->getLatitude())); // spacing
-    mergingCar.push_back(x.first->getLatitude());
-    mergingCar.push_back(x.first->getLongitude());
+    mergingCar.push_back(distanceEarth(RoadUserGPStoProcessedGPS(merging_car->getLongitude()),RoadUserGPStoProcessedGPS(merging_car->getLatitude()),RoadUserGPStoProcessedGPS(x.first->getLongitude()),RoadUserGPStoProcessedGPS(x.first->getLatitude()))); // spacing
+    mergingCar.push_back(RoadUserGPStoProcessedGPS(x.first->getLatitude()));
+    mergingCar.push_back(RoadUserGPStoProcessedGPS(x.first->getLongitude()));
     mergingCar.push_back(x.first->getLength());
     mergingCar.push_back(x.first->getWidth());
     mergingCar.push_back(RoadUserSpeedtoProcessedSpeed(x.first->getSpeed()));
     mergingCar.push_back(x.first->getAcceleration());
-    mergingCar.push_back(x.second->getLatitude());
-    mergingCar.push_back(x.second->getLongitude());
+    mergingCar.push_back(RoadUserGPStoProcessedGPS(x.second->getLatitude()));
+    mergingCar.push_back(RoadUserGPStoProcessedGPS(x.second->getLongitude()));
     mergingCar.push_back(x.second->getWidth());
     mergingCar.push_back(RoadUserSpeedtoProcessedSpeed(x.second->getSpeed()));
     mergingCar.push_back(x.second->getAcceleration());
-    mergingCar.push_back(distanceEarth(merging_car->getLongitude(),merging_car->getLatitude(),x.second->getLongitude(),x.second->getLatitude())); // spacing
+    mergingCar.push_back(distanceEarth(RoadUserGPStoProcessedGPS(merging_car->getLongitude()),RoadUserGPStoProcessedGPS(merging_car->getLatitude()),RoadUserGPStoProcessedGPS(x.second->getLongitude()),RoadUserGPStoProcessedGPS(x.second->getLatitude()))); // spacing
 		mergingCar.push_back(merging_car->getHeading());
 
   return mergingCar;
@@ -336,8 +343,8 @@ ManeuverRecommendation* calculatedTrajectories(RoadUser * mergingVehicle,at::Ten
 
 	Waypoint * waypoint = new Waypoint();
   waypoint->setTimestamp(timeCalculator.count() + (distanceEarth(mergingVehicle->getLatitude(),mergingVehicle->getLongitude(),calculated_n_1_states[0][0].item<float>(),calculated_n_1_states[0][1].item<float>())/mergingVehicle->getSpeed())*1000); //distance to mergeing point
-  waypoint->setLatitude(calculated_n_1_states[0][0].item<float>());
-  waypoint->setLongitude(calculated_n_1_states[0][1].item<float>());
+  waypoint->setLatitude(ProcessedGPStoRoadUserGPS(calculated_n_1_states[0][0].item<float>()));
+  waypoint->setLongitude(ProcessedGPStoRoadUserGPS(calculated_n_1_states[0][1].item<float>()));
   waypoint->setSpeed(ProcessedSpeedtoRoadUserSpeed(calculated_n_1_states[0][4].item<float>()));
   waypoint->setLanePosition(mergingVehicle->getLanePosition()+1);
   mergingManeuver->addWaypoint(waypoint);
@@ -354,8 +361,8 @@ ManeuverRecommendation* calculatedTrajectories(RoadUser * mergingVehicle,at::Ten
 
 		Waypoint * n_waypoint = new Waypoint();
 	  n_waypoint->setTimestamp(timeCalculator.count() + (distanceEarth(mergingVehicle->getLatitude(),mergingVehicle->getLongitude(),calculated_waypoint[0][0].item<float>(),calculated_waypoint[0][1].item<float>())/mergingVehicle->getSpeed())*1000); //distance to mergeing point
-	  n_waypoint->setLatitude(calculated_waypoint[0][0].item<float>());
-	  n_waypoint->setLongitude(calculated_waypoint[0][1].item<float>());
+	  n_waypoint->setLatitude(ProcessedGPStoRoadUserGPS(calculated_waypoint[0][0].item<float>()));
+	  n_waypoint->setLongitude(ProcessedGPStoRoadUserGPS(calculated_waypoint[0][1].item<float>()));
 	  n_waypoint->setSpeed(ProcessedSpeedtoRoadUserSpeed(calculated_waypoint[0][4].item<float>()));
 	  n_waypoint->setLanePosition(mergingVehicle->getLanePosition()+1);
 	  mergingManeuver->addWaypoint(n_waypoint);
