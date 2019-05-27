684e9832957ac562053650a892ec14435c611def
kvmk8371 <frederic.gardes@orange.com>
2019-05-27 14:46:36 +0200

    logger : add a simple logger
    Catch the method from detection_interface
    "write_to_log", and share it as "write"
    into the "logger" namespace.

---
+++ to/logger.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Frédéric Gardes<frederic.gardes@orange.com> on 5/27/19.
+//
+
+#include "logger.h"
+
+#include <fstream>
+
+auto logger::write(const std::string &text) -> void {
+    std::ofstream log_file("logs/to_logs.log", std::ios_base::out | std::ios_base::app);
+    log_file << text << std::endl;
+}
---
+++ to/logger.h
@@ -0,0 +1,14 @@
+//
+// Created by Frédéric Gardes<frederic.gardes@orange.com> on 5/27/19.
+//
+
+#ifndef COMMUNICATION_LAYER_LOGGER_H
+#define COMMUNICATION_LAYER_LOGGER_H
+
+#include <string>
+
+namespace logger {
+    auto write(const std::string &text) -> void;
+}
+
+#endif //COMMUNICATION_LAYER_LOGGER_H
